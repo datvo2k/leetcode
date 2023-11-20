0fd0f2a019003b046a5f4570c7beca21340cb8b0
Brian Vo <datvo2k@gmail.com>
2023-11-20 23:31:25 +0700

    update

--- MinStack.py
+++ MinStack.py
@@ -31,15 +31,21 @@ minStack.getMin(); // return -2
 class MinStack:
 
     def __init__(self):
+        self.stack = []
+        self.minimum = [float('inf')]
         
-
     def push(self, val: int) -> None:
-        
+        self.stack.append(val)
+        self.minimum.append(min(self.minimum[-1], val))
+
 
     def pop(self) -> None:
+        self.stack.pop()
+        self.minimum.pop()
         
 
     def top(self) -> int:
+        return self.stack[-1]
         
-
     def getMin(self) -> int:
+        return self.minimum[-1]
